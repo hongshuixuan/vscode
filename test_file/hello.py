8dc36b368969b62301cc63e2ed3b07a9e2f88676
hongshuixuan <2234381424@qq.com>
2020-08-12 22:01:59 +0800

    Add files via upload

---
+++ test_file/hello.py
@@ -0,0 +1,10 @@
+# '''
+# Author: your name
+# Date: 2020-08-12 21:55:31
+# LastEditTime: 2020-08-12 21:56:14
+# LastEditors: Please set LastEditors
+# Description: In User Settings Edit
+# FilePath: \vscode_py\test_file\hello.py
+# '''
+print("hello world!")
+print("hello world!")
